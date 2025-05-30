3e4f60f3afd21638491b409ce9fb19e044e7f9ee
softwareprymaerospace@gmail.com <softwareprymaerospace@gmail.com>
2025-05-30 13:05:54 +0530

    added the server in the folder

---
+++ Server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Import routes
+
+app.get('/', (req, res) => {
+     res.send("Welcome to the server of code collab");
+})
+
+
+
+app.listen(PORT, () => {
+     console.log(`server is running on http://localhost:${PORT}`);
+})
